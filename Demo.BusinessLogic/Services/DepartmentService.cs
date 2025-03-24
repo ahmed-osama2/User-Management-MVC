cd6e09f6f962e37dd2ced651b3e97a6aeb67301e
ahmed-osama2 <ahmedosama12467@gmail.com>
2025-03-25 01:18:34 +0200

    error

--- Demo.BusinessLogic/Services/DepartmentService.cs
+++ Demo.BusinessLogic/Services/DepartmentService.cs
@@ -3,17 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Demo.BusinessLogic.DataTransferObjects;
 using Demo.DataAccess.Repositories;
 
 namespace Demo.BusinessLogic.Services
 {
-    internal class DepartmentService
+    internal class DepartmentService(IDepartmentRepository _departmentRepository)
     {
-        private readonly IDepartmentRepository departmentRepository;
+        private readonly IDepartmentRepository departmentRepository = _departmentRepository;
 
-        public DepartmentService(IDepartmentRepository departmentRepository) //  01-injection
+        public IEnumerable<DepartmentDto> GetAllDepartments()
         {
-            this.departmentRepository = departmentRepository;
-        } 
+            var departments = _departmentRepository.GetAll();
+
+            var departmentsToReturn = departments.Select(D => new DepartmentDto()
+            {
+                DeptId = D.Id,
+                Name = D.Name,
+                Code = D.Code,
+                Description = D.Description,
+                DateOfCreation = DateOnly.FromDateTime(D.CreatedOn)
+
+
+            });
+            
+            return departmentsToReturn;
+
+
+        }
     }
+
+
+        
+
+
+
 }
