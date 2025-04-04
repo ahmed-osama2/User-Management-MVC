00b0276499959ed46696997c79e77232bfbf8972
ahmed-osama2 <ahmedosama12467@gmail.com>
2025-04-04 17:52:58 +0200

    updaet

--- Demo.DataAccess/models/BaseEntity.cs
+++ Demo.DataAccess/models/BaseEntity.cs
@@ -11,7 +11,7 @@ namespace Demo.DataAccess.models
         public int Id { get; set; } // PK
         public int CreatedBy { get; set; } // User Id
 
-        public DateTime? CreatedOn { get; set; }
+        public DateTime CreatedOn { get; set; }
 
         public int LastModifiedBy { get; set; } // User ID
         public DateTime? LastModifiedOn { get; set; }
